a34b65fb3e4979abc1ef1fd14b4cb7baa67902e2
github4mkandula <github4mkandula@users.noreply.github.com>
2020-06-08 21:35:37 -0500

    DBConnection with getConnection(),crearteStatement()

---
+++ DBConnection.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+def getConnection(un,pw,dbname):
+
+    try:
+        conn = mysql.connector.connect(host="127.0.0.1", user=un passwd=pw, database=dbname)
+        return conn
+    except Exception as e:
+        print('unable to connect')
+        return e
+
+def createStatement(conn):
+    try:
+
+        statement = conn.cursor()
+        print(statement)
+        return statement
+    except Exception as e
+        print('error in creating statement')
+        return e
+
+
